33fda03c833c8bbd53b0a8458e1e02f56a56b752
matthewb2 <bestsciencekwabe@gmail.com>
2026-05-12 13:52:19 +0900

    fix: image error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'mksolution.dothome.co.kr',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
